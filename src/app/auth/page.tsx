ec1ce2ee38b94196a6385c4f6033c1a6bea1ca36
Mh lashgari <m.h.lashgari98@gmail.com>
2025-02-06 22:11:51 +0330

    fix: build error

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { LoginForm } from "@/components/login-form";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-neutral-100 p-6 md:p-10 dark:bg-neutral-800">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          {" "}
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
