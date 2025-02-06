ff615f3c516468e55fe35e33c571ba9c88d0734f
Mh lashgari <m.h.lashgari98@gmail.com>
2025-02-06 22:14:31 +0330

    feat: add lamps to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { LampContainer } from "@/components/ui/lamp";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Click me</Button>
-    </div>
+    <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        HI MAMAD <br />
+        SAFAR
+      </motion.h1>
+    </LampContainer>
   );
 }
